9a63c3540534db888ce42f99673b3c9abbc5e580
Slihfer <49499110+Slihfer@users.noreply.github.com>
2022-10-09 16:46:56 +0200

    replace vue example setup

---
+++ src/client/graphics/coordinates.ts
@@ -0,0 +1,5 @@
+import { Vector2 } from '@/shared/framework/math/Vector2';
+
+function worldToCanvas(coordinates: Vector2) {
+  // return new Vector2()
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
-import { createApp } from "vue";
-import App from "./client/App.vue";
-import router from "./client/router";
-
-import "./app/assets/main.css";
+import { createApp } from 'vue';
+import App from './client/App.vue';
+import { Chronology } from './shared/framework/chronology/Chronology';
+import { Time } from './shared/framework/simulation/Time';
+import { Game } from './shared/game/state/Game';
 
 const app = createApp(App);
+let chronology: Chronology<Game>
 
-app.use(router);
+app.mount('#app');
 
-app.mount("#app");
+
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement
+const drawCtx = canvas.getContext('2d')
+
+function drawGame() {
+  Time.update()
+  let frame = chronology.get(Time.current)
+
+  // drawCtx?.ellipse()
+
+  window.requestAnimationFrame(drawGame)
+}
